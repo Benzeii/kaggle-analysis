ea41dbc90428e370488824ec0f13093add59cbbd
Ben <benjaminperry128@gmail.com>
2025-07-10 19:00:31 +0100

    Added inital data inspecting

--- analysis.py
+++ analysis.py
@@ -1,5 +1,15 @@
 import pandas as pd
+import numpy
+import sklearn
+import sqlite3
 
 df = pd.read_csv('tmdb_5000_movies.csv')
 
-print(df)
+# Print first 5 rows
+print(df.head())
+
+# Print missing values per column
+print(df.isnull().sum())
+
+# Print data types of columns
+print(df.dtypes)
